2af69143872359502033599f28dd8c31f88154aa
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-12 23:53:01 +0300

    קה

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-(function (angular) {
-  "use strict";
-  angular.module("heroApp", []);
-})(window.angular);
-
-/*
-  Copyright 2022 Google LLC. All Rights Reserved.
-  Use of this source code is governed by an MIT-style license that
-  can be found in the LICENSE file at http://angular.io/license
-  */
-
-const viewer = new Cesium.viewer("cesiumContainer");
+angular.module("heroApp", []);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Cesium Template App
+
+This is a template for making Cesium apps. Click on `index.html` on the left to see the code.
+
+New to Cesium? Visit our [learning center](http://cesium.com/docs) to get started.
